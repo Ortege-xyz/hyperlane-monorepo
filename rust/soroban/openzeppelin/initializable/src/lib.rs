2308b4fbc93fd0e87901c07b93edb97457b33639
Pedro Antonio <37405304+prodesert22@users.noreply.github.com>
2023-10-24 09:56:08 -0400

    feat(soroban): OwnableUpgradeable, Address & IMessageRecipient (#29)
    * fix: wip for initialized contract
    
    * fix : split initializer to works same of solidity
    
    * feat: added ownableupgradeable
    
    * fix: wip for initializable test
    
    * fix: Contract Changes
    
    * feat: added address lib to check if is a contract
    
    * feat: added imessage recipient interface
    
    * feat: added after_initializer function
    
    ---------
    
    Co-authored-by: Sarju Hansaliya <sarju.hansaliya@gmail.com>

--- rust/soroban/openzeppelin/initializable/src/lib.rs
+++ rust/soroban/openzeppelin/initializable/src/lib.rs
@@ -44,6 +44,7 @@ pub enum ContractError {
 const COUNTER: Symbol = symbol_short!("COUNTER");
 
 //TODO: check which storage actullay need env storage or stuct storage
+#[derive(Clone)]
 pub struct Initializable {
     storage: InitializableStorage,
 }
@@ -83,6 +84,13 @@ impl Initializable {
             self.storage.initializing = true;
         }
 
+        Ok(())
+    }
+
+    // This fuction is the continuation of initializer fuction because in 
+    // solidity the modifier have the special param '_;'
+    // https://www.educative.io/answers/what-is-in-solidity
+    pub fn after_initializer(&mut self, env: Env){
         self.storage.initializing = false;
 
         env.events().publish((COUNTER, symbol_short!("initial")), 1);
