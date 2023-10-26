b4036af715a36b28666377ac06aa1d79ed6fd498
Pedro <pedroantonioppms@gmail.com>
2023-10-26 12:11:30 -0400

    feat: added quote_gas_payment function

--- rust/soroban/contracts/interchain-gas-paymaster/src/lib.rs
+++ rust/soroban/contracts/interchain-gas-paymaster/src/lib.rs
@@ -1,12 +1,14 @@
 #![no_std]
 use soroban_sdk::{
-    contract, contractimpl, contracttype, symbol_short, Address, Bytes, Env, Symbol,
+    contract, contractimpl, contracttype, symbol_short, Address, Bytes, Env, FromVal, Symbol, U256,
 };
 use storage_gas_oracle::StorageGasOracleClient;
 
 const BENEFICIARY: Symbol = symbol_short!("BENEFICIA");
 const GASCONFIG: Symbol = symbol_short!("GASCONFIG");
 const SET: Symbol = symbol_short!("SET");
+
+const TOKEN_EXCHANGE_RATE_SCALE: u128 = 10000000000; //1e10
 
 #[contract]
 pub struct InterchainGasPaymaster;
@@ -36,6 +38,38 @@ impl InterchainGasPaymaster {
             .get(&DataKey::Beneficiary)
             .unwrap_or(0);
     }
+
+    /**
+     * @notice Quotes the amount of native tokens to pay for interchain gas.
+     * @param _destinationDomain The domain of the message's destination chain.
+     * @param _gasLimit The amount of destination gas to pay for.
+     * @return The amount of native tokens required to pay for interchain gas.
+     */
+    pub fn quote_gas_payment(env: Env, _destination_domain: u32, _gas_limit: U256) -> U256{
+        // Get the gas data for the destination domain.
+        let (_token_exchangerate, _gas_price) =
+            Self::get_exchangerate_and_gasprice(env.clone(), _destination_domain);
+
+        // The total cost quoted in destination chain's native token
+        let _destination_gas_cost = _gas_limit.mul(&U256::from_be_bytes(
+            &env,
+            &Bytes::from_array(&env, &_gas_price.to_be_bytes()),
+        ));
+
+        // Convert to the local native token.
+        let amount = _destination_gas_cost
+            .mul(&U256::from_be_bytes(
+                &env,
+                &Bytes::from_array(&env, &_gas_price.to_be_bytes()),
+            ))
+            .div(&U256::from_be_bytes(
+                &env,
+                &Bytes::from_array(&env, &TOKEN_EXCHANGE_RATE_SCALE.to_be_bytes()),
+            ));
+
+        return amount;
+    }
+
     /**
      * @notice Gets the token exchange rate and gas price from the configured gas oracle
      * for a given destination domain.
